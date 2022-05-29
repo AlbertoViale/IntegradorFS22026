d8ea46630d12ce8d2d60698cd69bc4100d3c324e
AlbertoViale <albertoviale2015@gmail.com>
2022-05-29 20:48:01 -0300

    agregamos los archivos de javascript

---
+++ tickets.js
@@ -0,0 +1,43 @@
+
+const valorTicket=200;
+
+function boton(){
+  let descuento=0;
+
+  const nombre=document.querySelector('#inombre').value;
+  const apellido=document.querySelector('#iapellido').value;
+  const correo=document.querySelector('#mail').value
+  if(nombre!='' && apellido!='' && correo!=''){
+
+    let ticketCant=document.getElementById('idInputCantidad').value;
+    const selector=document.getElementById('inputState').value;
+    //console.log(selector);
+    if(selector=='Estudiante'){
+       descuento=valorTicket*parseInt(ticketCant)* 0.80;
+    }
+    else if(selector=='Junior'){
+      descuento=valorTicket*parseInt(ticketCant)* 0.15;
+
+    }
+    else
+       descuento=valorTicket*parseInt(ticketCant)* 0.50;
+  
+    let suPago=parseInt(ticketCant)*valorTicket-descuento;
+    document.getElementById('idtotal').innerHTML='Su pago es $:'+suPago;
+  }
+  else{
+    document.getElementById('idtotal').innerHTML='Faltan algunos datos';
+  }
+}  
+  function borrar(){
+    document.querySelector('#iform').reset()
+  }
+ 
+
+
+///console.log(ticketCant)
+  //console.log(typeof(ticketCant));
+  //console.log(suPago);
+  //console.log(typeof(suPago));
+  
+ 
